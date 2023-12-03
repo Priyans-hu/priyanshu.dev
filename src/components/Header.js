7d5242346b0550b16dcade60cb83a97e22202915
priyans-hu <gargpriyanshu2004@gmail.com>
2023-12-03 17:08:26 +0530

    updated header navigation links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../styles/Header.css'
 
 export default function Header() {
     const [menuBtn, setMenuBtn] = useState(0);
-    
+
     const menuToggleHandler = (e) => {
         if (menuBtn) {
             setMenuBtn(0);
@@ -18,13 +18,15 @@ export default function Header() {
         <header>
             <div className='navTop'>
                 <div className='headerContainer'>
-                    <h1 className='headerMainHeading'>Priyanshu</h1>
+                    <a href='/'>
+                        <h1 className='headerMainHeading'>Priyanshu</h1>
+                    </a>
                     <div className='headerNavigationContainer'>
                         <i className='fa-solid fa-bars menuToggleBtn' onClick={menuToggleHandler}></i>
                         <ul className='headerNavigationList'>
-                            <li className='headerNavigationListItem'><a href='/'>About</a></li>
-                            <li className='headerNavigationListItem'><a href='/'>Work</a></li>
-                            <li className='headerNavigationListItem'><a href='/'>Contact</a></li>
+                            <li className='headerNavigationListItem'><a href='/about'>About</a></li>
+                            <li className='headerNavigationListItem'><a href='/work'>Work</a></li>
+                            <li className='headerNavigationListItem'><a href='/contact'>Contact</a></li>
                         </ul>
                     </div>
                 </div>
