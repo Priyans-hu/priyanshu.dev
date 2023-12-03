516a0211c7281922c26a7bf5687695b473da8dbf
priyans-hu <gargpriyanshu2004@gmail.com>
2023-12-03 16:49:41 +0530

    added assets, completed intro page, fixed header

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Landing.css';
+
+const Landing = () => {
+    return (
+        <section className='landingContainer'>
+            <div className="backgroundContainer"></div>
+            <div className="contentContainer">
+                <h1>Designer <span className=''>&</span> Developer.</h1>
+            </div>
+        </section>
+    )
+};
+
+export default Landing;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import '../styles/HomePage.css';
+import Landing from '../components/Landing';
+import Introduction from '../components/Introduction';
+import Projects from '../components/Projects';
 const HomePage = () => {
     return (
-        <div>
-
-        </div>
+        <main>
+            <div className='homepageMainContainer'>
+                <Landing />
+                <Introduction />
+                <Projects />
+            </div>
+        </main>
     )
 }
 
