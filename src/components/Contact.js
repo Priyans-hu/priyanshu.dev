048cbaec5282c012da8f866f6c8d51a13169ffe6
priyans-hu <gargpriyanshu2004@gmail.com>
2023-12-07 12:26:01 +0530

    updated background and css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Contact.css'
 
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [sub, setSub] = useState('');
+    const [additional, setAdditional] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Details: Name - ${name}, Email - ${email}, Subject - ${sub}, Additional - ${additional || 'N/A'}`);
+    }    
+
     return (
         <section className='contactContainer'>
             <div className='backgroundContainer'></div>
@@ -11,7 +21,7 @@ const Contact = () => {
                         <h2>Let's have a coffee together!</h2>
                         <div className="contactMailMe">
                             <span>
-                                <i class="fa-regular fa-envelope"></i>
+                                <i className="fa-regular fa-envelope"></i>
                             </span>
                             <div>
                                 <p>Mail me at:</p>
@@ -21,13 +31,42 @@ const Contact = () => {
                     </div>
                     <div className="contactRight">
                         <h4>Send a message</h4>
-                        <form action="">
-                            <input type="text" name="name" id="name" placeholder='Full Name' />
-                            <input type="email" name="email" id="email" placeholder='Email ' />
-                            <input type="text" name="subject" id="subject" placeholder='Subject' />
+                        <form onSubmit={(e) => handleSubmit(e)}>
+                            <input 
+                                type="text" 
+                                name="name" 
+                                id="name" 
+                                placeholder='Full Name' 
+                                value={name} 
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <input 
+                                type="email" 
+                                name="email" 
+                                id="email" 
+                                placeholder='Email' 
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <input 
+                                type="text" 
+                                name="subject" 
+                                id="subject" 
+                                placeholder='Subject' 
+                                value={sub} 
+                                onChange={(e) => setSub(e.target.value)}
+                            />
                             <h5>Tell me more about your project</h5>
-                            <textarea name="more" id="more" cols="30" rows="4"></textarea>
-                            <button>Send message</button>
+                            <textarea 
+                                name="more" 
+                                id="more" 
+                                cols="30" 
+                                rows="4" 
+                                value={additional} 
+                                onChange={(e) => setAdditional(e.target.value)}
+                            >
+                            </textarea>
+                            <button type='submit'>Send message</button>
                         </form>
                     </div>
                 </div>
