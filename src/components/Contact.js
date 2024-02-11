e970fe03288f142cb13bcea89a1b3cd57bc0a30c
priyans-hu <gargpriyanshu2004@gmail.com>
2024-02-12 03:26:40 +0530

    managed code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../styles/Contact.css'
-import axios from 'axios'
+// import axios from 'axios'
 
 const Contact = () => {
     const [name, setName] = useState('');
