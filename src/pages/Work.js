f5f19356a6debeb99e0c2e611b07e7279ab710a8
priyans-hu <gargpriyanshu2004@gmail.com>
2024-06-12 02:36:10 +0530

    converted react app to next app

---
+++ src/pages/Work.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ThreeDCard } from '../../newapp/src/components/WorkCard1';
+
+const Work = () => {
+    return (
+        <main>
+            <div className='homepageMainContainer'>
+                <ThreeDCard />
+            </div>
+        </main>
+    )
+}
+
+export default Work
