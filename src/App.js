0b5c095cac65d87c65f6a630cd46e9d4b42f0080
priyans-hu <gargpriyanshu2004@gmail.com>
2024-06-12 01:59:47 +0530

    started new design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-// Importing react libraries
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Importing components
-import Header from './components/Header';
 import HomePage from './pages/HomePage';
-import Contact from './components/Contact';
-import Introduction from './components/Introduction';
-import Footer from './components/Footer';
-import Projects from './components/Projects';
 
 function App() {
     return (
         <div className="App">
-            <Header />
             <Router>
                 <Routes>
                     <Route path='/' exact Component={HomePage} />
-                    <Route path='/about' exact Component={Introduction} />
-                    <Route path='/work' exact Component={Projects} />
-                    <Route path='/contact' exact Component={Contact} />
                 </Routes>
             </Router>
-            <Footer />
         </div>
     );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import '../styles/HomePage.css';
 import Landing from '../components/Landing';
-import Introduction from '../components/Introduction';
-import Projects from '../components/Projects';
-import Contact from '../components/Contact';
+
 const HomePage = () => {
     return (
         <main>
             <div className='homepageMainContainer'>
                 <Landing />
-                <Introduction />
-                <Projects />
-                <Contact />
             </div>
         </main>
     )
